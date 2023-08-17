610f8cdc8d331c00052c25bd89659252e116cdb0
Flawersaurio <142053653+Flawersaurio@users.noreply.github.com>
2023-08-17 15:46:45 -0500

    Sesion3
    a

---
+++ chambita/chambita/color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chambita
+{
+    public class color
+    {
+        int Rojo;
+        int Verde;
+        int Azul;
+    }
+}
---
+++ chambita/chambita/mochila.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace chambita
+{
+    public class mochila
+    {
+        int NumDeBolsas;
+        string Tamanio;
+        float Precio;
+        string Disenio;
+        string Material;
+        int NumDeLlaveros;
+        color color;
+    }
+}
