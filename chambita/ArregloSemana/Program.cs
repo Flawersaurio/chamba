fd93b42be519efae2c8faa455ad7a7ca4e012915
Flawersaurio <142053653+Flawersaurio@users.noreply.github.com>
2023-09-08 13:09:55 -0600

    a
    nosewei

---
+++ chambita/ArregloSemana/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArregloSemana
+{
+    class ArregloSemana
+    {
+        static void Main(string[] args)
+        {
+            //Arreglo de strings con declaracion explicita
+            public string[] diasSemana 
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
