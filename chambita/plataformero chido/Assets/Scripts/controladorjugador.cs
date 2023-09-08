dfc674e00a9ea825cc43c87a2411f11b91e593fb
Flawersaurio <142053653+Flawersaurio@users.noreply.github.com>
2023-09-08 14:48:14 -0600

    caminar
    se implemento la caminasion

---
+++ chambita/plataformero chido/Assets/Scripts/controladorjugador.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorjugador : MonoBehaviour
+{
+    public float velocidadCaminar = 3;
+    private Rigidbody2D miCuerpo;
+    private Animator miAnimador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Al atributo MiCuerpo le asigno el componente rigidbody 2d de este  pesonaje 
+        miCuerpo = GetComponent<Rigidbody2D>();
+        miAnimador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float velActualVert = miCuerpo.velocity.y;
+
+        //Leo si el jugador esta presionando un eje horizontal en las flechas
+        float movHoriz = Input.GetAxis("Horizontal");
+        if(movHoriz > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);                           
+            miCuerpo.velocity = new Vector3(velocidadCaminar, velActualVert, 0);
+            miAnimador.SetBool("Caminando", true);
+
+        }
+        else if (movHoriz < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            miCuerpo.velocity = new Vector3(-velocidadCaminar, velActualVert, 0);
+            miAnimador.SetBool("Caminando", true);
+
+        }
+        else
+        {
+
+            miCuerpo.velocity = new Vector3(0, velActualVert, 0);
+            miAnimador.SetBool("Caminando", false);
+
+        }
+
+       
+    }
+}
